21745dedbfdcbe0b7d1dd7bccb23f7794845b6e7
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2026-07-16 13:40:05 -0400

    refactor: type cleanup_all_queues rows as QueueCleanup TypedDict

--- django_absurd/cleanup.py
+++ django_absurd/cleanup.py
@@ -1,13 +1,19 @@
+import typing as t
+
 from django.core.exceptions import ImproperlyConfigured
 from django.db import connections
 from django.db.utils import ProgrammingError
 
 from django_absurd.queues import resolve_absurd_database
 
-CLEANUP_COLUMNS = ("queue_name", "tasks_deleted", "events_deleted")
+
+class QueueCleanup(t.TypedDict):
+    queue_name: str
+    tasks_deleted: int
+    events_deleted: int
 
 
-def cleanup_all_queues() -> list[dict]:
+def cleanup_all_queues() -> list[QueueCleanup]:
     using = resolve_absurd_database()
     try:
         with connections[using].cursor() as cur:
@@ -19,4 +25,7 @@ def cleanup_all_queues() -> list[dict]:
     except ProgrammingError as exc:
         msg = "Absurd schema is not installed. Run: manage.py migrate"
         raise ImproperlyConfigured(msg) from exc
-    return [dict(zip(CLEANUP_COLUMNS, row, strict=True)) for row in rows]
+    return [
+        QueueCleanup(queue_name=queue_name, tasks_deleted=tasks, events_deleted=events)
+        for queue_name, tasks, events in rows
+    ]
